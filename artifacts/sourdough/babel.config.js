807c1c1ae70f2988e2cf6de16de289b0b49264a3
waterviewbh <waterviewbakehouse@gmail.com>
2026-07-18 12:56:35 -0600

    Additions and changes for app-1.1.0; webapp-0.3.3

--- artifacts/sourdough/babel.config.js
+++ artifacts/sourdough/babel.config.js
@@ -1,6 +1,19 @@
 module.exports = function (api) {
   api.cache(true);
+
+  // Check if the current bundler execution is targeting the web platform
+  const isWeb = process.env.EXPO_PUBLIC_PLATFORM === 'web';
+
   return {
-    presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
+    presets: [
+      [
+        "babel-preset-expo",
+        {
+          unstable_transformImportMeta: true,
+          // Explicitly turn off the React Compiler ONLY when compiling for the web browser
+          reactCompiler: isWeb ? false : true
+        }
+      ]
+    ],
   };
 };
